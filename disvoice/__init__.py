6942812531527ebd30e421317ccf332b1cb538ca
Ivan.Danylenko <Ivan.Danylenko@pl.ey.com>
2024-08-08 12:22:21 +0000

    Removed  features due to the imposibility of resolving errors

--- disvoice/__init__.py
+++ disvoice/__init__.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-
-
 from .utils import *
 from .script_mananger import script_manager
-
 
 # from . import (glottal, phonation, phonological, prosody, replearning, articulation)
 from .glottal import Glottal
@@ -11,8 +7,5 @@ from .phonation import Phonation
 from .prosody import Prosody
 from .replearning import RepLearning
 from .articulation import Articulation
-
-
-
 
 __all__=['Glottal', 'Phonation', 'Articulation', 'Prosody', 'Phonological', 'RepLearning']
